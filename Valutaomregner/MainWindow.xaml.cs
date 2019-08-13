b88a88d7de81d360798d5503f92564f9a1c59091
Jonas <jona345e@edu.sde.dk>
2019-08-13 12:43:20 +0200

    1.0

--- Valutaomregner/MainWindow.xaml.cs
+++ Valutaomregner/MainWindow.xaml.cs
@@ -47,7 +47,8 @@ namespace Valutaomregner
                 combo1.Items.Add(newItem1);
                 combo2.Items.Add(newItem2);
             }
-            
+            v1 = Getvaluta(1);
+            v2 = Getvaluta(2);
         }
 
         private void ChangeK(object sender, EventArgs e)
@@ -55,14 +56,96 @@ namespace Valutaomregner
             ComboBox box = sender as ComboBox;
             if(box.Name == "combo1")
             {
-                //v1 = float.Parse(((ComboBoxItem)combo1.SelectedItem).Tag.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                v1 = Getvaluta(1);
             }
-            else if(box.Name == "combo1")
+            else if(box.Name == "combo2")
             {
-                //v2 = float.Parse(((ComboBoxItem)combo2.SelectedItem).Tag.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                v2 = Getvaluta(2);
+            }
+            if (box.Name == "combo1")
+            {
+                Currency(2);
+            }
+            else if (box.Name == "combo2")
+            {
+                Currency(1);
             }
 
+        }
+        public float Getvaluta(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    return float.Parse(((ComboBoxItem)combo1.SelectedItem).Tag.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                case 2:
+                    return float.Parse(((ComboBoxItem)combo2.SelectedItem).Tag.ToString(), System.Globalization.CultureInfo.InvariantCulture);
+                default:
+                    return 0;
+                  
 
+            }
+        }
+
+
+        
+
+        private void Value_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+
+            TextBox value = sender as TextBox;
+            if(value.Name == "value1")
+            {
+                Currency(1);
+            }
+            else if (value.Name == "value2")
+            {
+                Currency(2);
+            }
+
+              
+            
+        }
+        public void Currency(int id)
+        {
+            bool is1Num = float.TryParse(value1.Text, out float num1);
+            bool is2Num = float.TryParse(value2.Text, out float num2);
+
+            if ((!is1Num) && (id == 1))
+            {
+                value2.Text = "not a number";
+            }
+            else if ((!is2Num) && (id == 2))
+            {
+                value1.Text = "not a number";
+            }
+            else
+            {
+                
+                    if (id == 1)
+                    {
+                        value2.Text = (v1/v2*num1).ToString();
+
+                    }
+                    else if (id == 2)
+                    {
+                        value1.Text = (v2 / v1 * num2).ToString();
+
+                    }
+                
+            }
+        }
+
+        private void SwitchValue(object sender, MouseButtonEventArgs e)
+        {
+            var a = value1.Text;
+            var b = value2.Text;
+            var c = combo1.SelectedIndex;
+            var d = combo2.SelectedIndex;
+            value1.Text = b;
+            value2.Text = a;
+            combo1.SelectedIndex = d;
+            combo2.SelectedIndex = c;
         }
     }
 
